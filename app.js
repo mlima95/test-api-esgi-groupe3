fbe8f5a5d0bd08c576b2b13b0726bd99021e8ccb
mlima95 <milan.lima@hotmail.fr>
2022-05-12 14:46:33 +0200

    fix import

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const {default: crud} = require("express-crud-router");
 const {default: sequelizeCrud} = require("express-crud-router-sequelize-v6-connector");
-const {User} = require("./models");
+const {User, Burger, Order} = require("./models");
 
 const app = express();
 app.use(express.json());
