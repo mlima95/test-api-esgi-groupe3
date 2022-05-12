cc2bba2d4955e5900d42d803cf007d49cf3679f2
Cornivulet <22870013+Cornivulet@users.noreply.github.com>
2022-05-12 15:56:36 +0200

    Added tests for Burger entity

--- __tests__/burger.spec.js
+++ __tests__/burger.spec.js
@@ -13,4 +13,44 @@ describe("test Burger API", () => {
   jest.spyOn(models.Burger, 'create');
   expect(response.statusCode).toBe(200);
   })
+
+  it("should return a burger", async () => {
+    const response = await request(app).get("/burgers/1");
+    expect(response.statusCode).toBe(200);
+  })
+
+  it("should create a burger", async () => {
+    const response = await request(app).post("/burgers").send({name: "MagBurger", price: 10.0});
+    expect(response.statusCode).toBe(201);
+  });
+
+  it("should update a burger", async () => {
+    const response = await request(app).put("/burgers/1").send({name: "MagueBurger", price: 11.0});
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('should update partially a burger', async () => {
+    const response = await request(app).patch("/burgers/1").send({name: "MagBurger"});
+    expect(response.statusCode).toBe(200);
+  });
+
+  it("should delete a burger", async () => {
+    const response = await request(app).delete("/burgers/1");
+    expect(response.statusCode).toBe(200);
+  });
+
+  it("should return a 404 when burger is not found", async () => {
+    const response = await request(app).get("/burgers/42069");
+    expect(response.statusCode).toBe(404);
+  });
+
+  it("should return an error when only burger name is sent", async () => {
+    const response = await request(app).post("/burgers/1").send({name: "MagBurger"});
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("should return an error when only burger price is sent", async () => {
+    const response = await request(app).post("/burgers/1").send({price: 10.0});
+    expect(response.statusCode).toBe(400);
+  });
 });
